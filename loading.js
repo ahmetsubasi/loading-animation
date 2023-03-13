6ee98bc007c7133ba444d9724f45d8273b7c27de
Ahmet Subasi <ahmttsubasii@gmail.com>
2023-03-13 12:29:50 +0300

    loading animation

---
+++ loading.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('.container');
+percent = document.querySelector('#percent');
+
+let perVal = 0;
+
+let increament = setInterval(() => {
+    perVal++;
+    percent.textContent = `${perVal} %`;
+    
+    if(perVal == 100){
+        clearInterval(increament)
+        container.classList.remove('active')
+    }
+
+}, 100)
